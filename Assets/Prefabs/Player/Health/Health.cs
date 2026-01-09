0e58d8763373a49a9afbd895f16744e9c6884462
wenzzzel <wenzzzel@live.se>
2026-01-09 16:05:18 +0100

    Created a healthbar mvp

---
+++ Assets/Prefabs/Player/Health/Health.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [Header("Health Settings")]
+    [SerializeField] private int maxHealth = 100;
+
+    [Header("References to other scripts")]
+    [SerializeField] private HealthBar healthBarScript;
+
+    public int currentHealth;
+
+#region Lifecycle calls
+
+    void Start()
+    {
+        if (healthBarScript == null)
+        {
+            Debug.LogError("HealthBar script reference not assigned in Health script.");
+        }
+
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        
+    }
+
+#endregion
+#region Private methods
+
+    private void Die()
+    {
+        Debug.Log("Player has died.");
+        // Add respawn or game over logic here
+    }
+
+#endregion
+#region Public methods
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        Debug.Log($"Player took {damageAmount} damage. Current health: {currentHealth}");
+
+        healthBarScript.UpdateHealthBar(damageAmount);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+#endregion
+}
---
+++ Assets/Prefabs/Player/Health/HealthBar/HealthBar.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class HealthBar : MonoBehaviour
+{
+    [Header("Visual Settings")]
+    [SerializeField] private float fullHealthSize = 10f;
+    [SerializeField] private float stepHealthSize = 0.1f;
+
+    private SpriteRenderer spriteRenderer;
+
+#region Lifecycle calls
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        transform.localScale = new Vector3(fullHealthSize, transform.localScale.y, transform.localScale.z);
+    }
+
+    void Update()
+    {
+        
+    }
+
+#endregion
+#region Public methods
+
+    public void UpdateHealthBar(int damageAmount)
+    {
+        var newSize = Mathf.Max(0f, fullHealthSize - damageAmount * stepHealthSize);
+        transform.localScale = new Vector3(newSize, transform.localScale.y, transform.localScale.z);
+    }
+
+#endregion
+
+}
