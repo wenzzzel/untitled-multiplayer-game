5518216cb23dfa43ca3a4acb259ced86ce75c0b8
wenzzzel <wenzzzel@live.se>
2026-01-15 20:26:48 +0100

    A little cleanup in PlayerAnimation.cs

--- Assets/Prefabs/Player/PlayerAnimation.cs
+++ Assets/Prefabs/Player/PlayerAnimation.cs
@@ -7,6 +7,7 @@ public class PlayerAnimation : NetworkBehaviour
     private Animator animator;
     private NetworkVariable<bool> isMoving = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
 
+#region Lifecycle calls
 
     void Awake()
     {
@@ -15,11 +16,22 @@ public class PlayerAnimation : NetworkBehaviour
 
     void Update()
     {
+        AnimateMovementLocally();
+    }
+
+#endregion
+#region Private methods
+
+    private void AnimateMovementLocally()
+    {
         if (isMoving.Value)
             animator.Play("Warrior_Run_Blue 0");
         else
             animator.Play("Warrior_Idle_Blue");
     }
+
+#endregion
+#region Public methods
 
     public void AnimateMovement(bool moving)
     {
@@ -31,5 +43,7 @@ public class PlayerAnimation : NetworkBehaviour
         else
             animator.Play("Warrior_Idle_Blue");
     }
+
+#endregion
     
 }
